2913a9bb722d2b2022fca69f0ab557529a43bd2b
chia <chia9322@gmail.com>
2022-03-30 18:09:56 +0900

    fix decode json data bug

--- TravelCost/model/model.swift
+++ TravelCost/model/model.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
-func decodeJsonData<T: Decodable>(_ data: Data) -> T {
+func decodeJsonData<T: Decodable>(_ data: Data) -> T? {
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
-        fatalError()
+        print(error)
+        return nil
     }
 }
 
@@ -21,10 +22,11 @@ func fetchGasolinePrice() {
     if let url = URL(string: urlString) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
-                let gasolineData: GasolineData = decodeJsonData(data)
-                gasoline.regular = Double(gasolineData.price.regular)?.rounded() ?? gasoline.regular
-                gasoline.highOctane = Double(gasolineData.price.highOctane)?.rounded() ?? gasoline.highOctane
-                gasoline.lightOil = Double(gasolineData.price.lightOil)?.rounded() ?? gasoline.lightOil
+                if let gasolineData: GasolineData = decodeJsonData(data) {
+                    gasoline.regular = Double(gasolineData.price.regular)?.rounded() ?? gasoline.regular
+                    gasoline.highOctane = Double(gasolineData.price.highOctane)?.rounded() ?? gasoline.highOctane
+                    gasoline.lightOil = Double(gasolineData.price.lightOil)?.rounded() ?? gasoline.lightOil
+                }
             }
         }.resume()
     }
@@ -35,9 +37,10 @@ func fetchFuelEfficiency() {
     if let url = URL(string: urlString) {
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
-                let avgFuelEfficiencyData: AvgFuelEfficiencyData = decodeJsonData(data)
-                avgFuelEfficiency.normal = avgFuelEfficiencyData.fuel.normal
-                avgFuelEfficiency.light = avgFuelEfficiencyData.fuel.light
+                if let avgFuelEfficiencyData: AvgFuelEfficiencyData = decodeJsonData(data) {
+                    avgFuelEfficiency.normal = avgFuelEfficiencyData.fuel.normal
+                    avgFuelEfficiency.light = avgFuelEfficiencyData.fuel.light
+                }
             }
         }.resume()
     }
